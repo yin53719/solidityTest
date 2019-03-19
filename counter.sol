31b130c63a9ba981e854a618f9a7af6ed30e8804
hardy yin <yin53719@qq.com>
2019-03-19 23:55:50 +0800

    Create counter.sol

---
+++ counter.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.24;
+
+contract Counter{
+    uint num;
+
+    constructor(){
+
+        num = 0;
+    }
+    function increment() public{
+        num += 1;
+    }
+
+    function getNum view returns(uint){
+        return num;
+    }
+
+}
