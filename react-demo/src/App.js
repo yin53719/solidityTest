daf4743142ebc399a6feb0030382491657d70413
yin53719 <yin53719@qq.com>
2019-03-25 15:39:38 +0800

    11

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import TruffleContract from 'truffle-contract';
-import AdoptionJson from '../truffle/build/contract/Adoption.json';
+import AdoptionJson from './truffle/build/contracts/Adoption.json';
 // import { isBuffer } from 'util'
 
 
@@ -18,11 +18,13 @@ class App extends Component {
 
   init(){
     if(typeof window.web3 !== "undefined"){
-        this.web3Provider = window.wen3.currentProvider;
+        this.web3Provider = window.web3.currentProvider;
+        this.web3 = new Web3(this.web3Provider)
 
     }else{
       alert('请安装metaMask')
     }
+    
   }
   render() {
     return (
