d038d06b8f83c9dc089e8011c1ca1e7a8c41a635
yin53719 <yin53719@qq.com>
2019-03-25 13:40:56 +0800

    111

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import TruffleContract from 'truffle-contract';
-import AdoptionJson from './truffle/build/contract/Adoption.json';
-import { isBuffer } from 'util'
+import AdoptionJson from '../truffle/build/contract/Adoption.json';
+// import { isBuffer } from 'util'
 
 
 class App extends Component {
@@ -27,20 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        111
       </div>
     );
   }
